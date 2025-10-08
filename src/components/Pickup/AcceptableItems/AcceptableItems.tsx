961f9ed707c70abfaa36965451e2ffca7431acda
Art <44448694+HFHT@users.noreply.github.com>
2025-10-08 16:37:57 -0700

    Update Acceptable Items

--- src/components/Pickup/AcceptableItems/AcceptableItems.tsx
+++ src/components/Pickup/AcceptableItems/AcceptableItems.tsx
@@ -2,7 +2,7 @@ import { Navigation } from "@/components/Controls"
 import { MainContext } from "@/contexts"
 import { useTheme } from "@/hooks"
 import { uniqueKey } from "@/utils"
-import { ScrollArea, SimpleGrid, Stack, Text, Title } from "@mantine/core"
+import { Divider, ScrollArea, SimpleGrid, Stack, Table, Text, Title } from "@mantine/core"
 import { useContext } from "react"
 
 export function AcceptableItems({ open }: { open: boolean }) {
@@ -17,7 +17,16 @@ export function AcceptableItems({ open }: { open: boolean }) {
       <Text>We do <u>not</u> accept:</Text>
       <ScrollArea h={mobile ? document.documentElement.clientHeight - 260 : ''} type="auto" mb='sm' >
         <SimpleGrid cols={mobile ? 1 : 2} spacing="xs" verticalSpacing="xs" aria-label={`We do not accept the following items: ${notAcceptedItems.join(', ')}`}>
+          <Text size='md' fw={700}>Box Springs</Text>
           {notAcceptedItems.map((im) => (
+            <Text key={uniqueKey()} size='sm'>{im}</Text>
+          ))}
+        </SimpleGrid>
+        <Divider mt='md' />
+        <Title size={22} order={3}>Requires prior approval</Title>
+        <Text>Please include photos:</Text>
+        <SimpleGrid cols={mobile ? 1 : 2} spacing="xs" verticalSpacing="xs" aria-label={`These items need prior approval, include photos: Bed frames, Entertainment Centers, Large Desks}`}>
+          {['Bed frames', 'Large Desks', 'Entertainment Centers'].map((im) => (
             <Text key={uniqueKey()} size='sm'>{im}</Text>
           ))}
         </SimpleGrid>
